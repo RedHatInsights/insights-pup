13151dbc65e3fd68532bc696d3fe16d58bd55466
Chris Duryee <cduryee@redhat.com>
2019-04-05 13:50:29 -0400

    remove unused code
    This area got refactored, we now just send the raw number of bytes and
    we rely on the parser to tell us which IPs are ipv4 vs v6.

--- tests/test_fact_extract.py
+++ tests/test_fact_extract.py
@@ -5,16 +5,6 @@ from pup.utils import fact_extract
 
 class TestFormatHelpers(unittest.TestCase):
 
-    def test_bytes_to_gb(self):
-        gb = fact_extract.bytes_to_gb(987654321)
-        self.assertEquals(gb, "1.0 GB")
-
-    def test_ipv4_ipv6_addresses(self):
-        ip_addresses = ['8.8.8.8', '3ffe:1900:4545:3:200:f8ff:fe21:67cf']
-        result = fact_extract.ipv4_ipv6_addresses(ip_addresses)
-        self.assertIn('8.8.8.8', result['ipv4_addresses'])
-        self.assertIn('3ffe:1900:4545:3:200:f8ff:fe21:67cf', result['ipv6_addresses'])
-
     def test_strip_bad_display_name(self):
         values = {"account": "12345", "metadata": {"display_name": "a", "non_empty_key": "non_empty_value"}}
         stripped_metadata = fact_extract._remove_bad_display_name(values["metadata"])
