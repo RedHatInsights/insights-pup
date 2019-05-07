70670a6e037592971b2a7d84940434042d48e871
Derek Horton <dehort@redhat.com>
2019-05-07 15:30:47 -0500

    Do not let failing to determine the commit date, cause the application to fail to start

--- pup/utils/get_commit_date.py
+++ pup/utils/get_commit_date.py
@@ -1,9 +1,18 @@
+import logging
 import requests
 
 COMMIT_BASE_URL = "https://api.github.com/repos/RedHatInsights/insights-pup/git/commits/"
 
+logger = logging.getLogger('advisor-pup')
+
 
 def get_commit_date(commit_id):
     url = COMMIT_BASE_URL + commit_id
-    response = requests.get(url).json()
-    return response['committer']['date']
+    try:
+        response = requests.get(url, timeout=0.5).json()
+        return response['committer']['date']
+    except Exception:
+        # Log the exception and keep going.  Do not let failing to
+        # determine the commit date stop the process from starting.
+        logger.exception("Error retrieving commit date from github")
+        return "unknown"
