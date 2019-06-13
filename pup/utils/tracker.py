e9b30ef771125af98fe96c070770fa6cdb412014
Stephen Adams <tsadams@gmail.com>
2019-06-13 15:48:26 -0400

    Add payload_tracker

---
+++ pup/utils/tracker.py
@@ -0,0 +1,27 @@
+import datetime
+
+from utils import configuration as config
+
+
+def get_time():
+    return str(datetime.datetime.now())
+
+
+def payload_tracker(request_id, account, status, status_msg):
+
+    payload_msg = {"topic": config.TRACKER_TOPIC}
+
+    payload_status = {
+        "service": "advisor-pup",
+        "status": status,
+        "status_msg": status_msg,
+        "account": account,
+        "request_id": request_id,
+        "payload_id": request_id,
+    }
+
+    payload_status["date"] = get_time()
+
+    payload_msg["msg"] = payload_status
+
+    return payload_msg
